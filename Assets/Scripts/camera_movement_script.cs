5407c215e148fa1d366a109d0a5473c5da73ab0c
Jukka Jylänki <jujjyl@gmail.com>
2026-07-30 22:45:28 +0300

    Fish rotation

--- Assets/Scripts/camera_movement_script.cs
+++ Assets/Scripts/camera_movement_script.cs
@@ -13,6 +13,7 @@ public class camera_movement_script : MonoBehaviour
     {
         // Debug for testing? move camera continuously.
         advance_camera_position(Time.deltaTime);
+        set_fish_error_delta(Mathf.Sin(Time.time));
     }
 
     static void advance_camera_position(float meters_delta)
@@ -20,5 +21,13 @@ public class camera_movement_script : MonoBehaviour
         GameObject g = GameObject.Find("game_camera");
         g.transform.position += new Vector3(0, 0, -meters_delta);
 
+        g = GameObject.Find("player fish model");
+        g.transform.position += new Vector3(0, 0, -meters_delta);
+    }
+
+    static void set_fish_error_delta(float delta)
+    {
+        GameObject g = GameObject.Find("player fish model");
+        g.transform.eulerAngles = new Vector3(0, delta * 45, 0);
     }
 }
